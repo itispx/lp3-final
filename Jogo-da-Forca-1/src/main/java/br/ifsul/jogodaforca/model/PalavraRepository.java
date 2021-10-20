429e01b8c76f265219cee7c1d29e63139709e635
Px_02 <guilhermeciotapx@outlook.com>
2021-10-19 21:48:01 -0300

    first commit

---
+++ Jogo-da-Forca-1/src/main/java/br/ifsul/jogodaforca/model/PalavraRepository.java
@@ -0,0 +1,7 @@
+package br.ifsul.jogodaforca.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PalavraRepository extends JpaRepository<Palavra, Integer> {
+
+}
